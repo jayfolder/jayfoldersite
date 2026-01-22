c2199f851abf4260a778be5dad45270e56abd0a4
jay-dotmp3 <jaygc.personal@gmail.com>
2026-01-22 13:30:50 +0100

    portfolio progress in college
    page now exists

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const links = {
     x:"https://x.com/jayf0lder",
     gj:"https://gamejolt.com/@jayfolder",
     yt:"https://www.youtube.com/@jayfolder",
-    itch:"https://jayfolder.itch.io/"
+    itch:"https://jayfolder.itch.io/",
+    port:"portfolio.html"
 }
    
 
-function move_page(a){
-  window.open(links[a], '_blank');
+function move_page(a, b){
+  window.open(links[a], '_'+b);
 }
 
 setTimeout(() => {
