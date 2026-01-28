8887d1aa358518d40ef8bd70f9d6e62462723fac
jay-dotmp3 <jaygc.personal@gmail.com>
2026-01-28 12:56:46 +0100

    holy shit so much progress!
    portfolio page is pretty much almost done, but i'm planning to implement React or Preact for the games and shit

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const links = {
     gj:"https://gamejolt.com/@jayfolder",
     yt:"https://www.youtube.com/@jayfolder",
     itch:"https://jayfolder.itch.io/",
-    port:"portfolio.html"
+    port:"portfolio.html",
+    home:"index.html",
+    weezer:"https://gamejolt.com/games/faaw/846454",
+    tbn:"https://gamejolt.com/games/TBN/850856",
+    dsmis3ep1:"https://gamejolt.com/games/dsmisarchive/830646",
+    test:"https://google.com"
 }
    
 
