da395ff0b46fd04266da3fad18829ef57526b483
jay <140921639+jayfolder@users.noreply.github.com>
2026-04-01 01:25:06 +0200

    update
    the update is pretty much done but i really wanna revise some code

--- src/components/script.js
+++ src/components/script.js
@@ -1,7 +1,34 @@
+var dm = false;
+const bgColor = ["#dfdffa", "#0a0a23"];
+const textColor = ["#0000ff", "#F1F1FF"]
+const dm_imgs = ["moon.svg", "sun.svg"];
+const root = document.documentElement;
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`
+    const parts = value.split(`; ${name}=`)
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift()
+    }
+    return null
+}
+
 window.move_page = function(a, b) {
   window.open(a, '_' + b)
 }
 
-setTimeout(() => {
-  document.body.style.overflow = "auto";
-}, 1100);
+window.onload = function(){
+  let dm_c = getCookie('dm');
+  dm = dm_c === "true";
+  document.getElementById("dm_icon").src = dm_imgs[+dm];
+  document.body.style.backgroundColor = bgColor[+dm];
+  root.style.setProperty('--main-color', textColor[+dm]);
+}
+
+window.darkMode = function() { 
+  dm = !dm;
+  document.getElementById("dm_icon").src = dm_imgs[+dm]
+  document.cookie = "dm="+dm+""
+  document.body.style.backgroundColor = bgColor[+dm];
+  root.style.setProperty('--main-color', textColor[+dm]);
+} 
