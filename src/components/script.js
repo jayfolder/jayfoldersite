ab549f256e657585e9c85c8217c20261c7a0b3c6
jay <140921639+jayfolder@users.noreply.github.com>
2026-05-20 19:46:01 +0200

    stopped being lazy

--- src/components/script.js
+++ src/components/script.js
@@ -1,9 +1,11 @@
 var dm = false;
-const bgColor = ["#dfdffa", "#0a0a23"];
+const bgColor = ["#F1F1FF", "#000074"];
 const textColor = ["#0000ff", "#F1F1FF"]
 const dm_imgs = ["moon.svg", "sun.svg"];
 const root = document.documentElement;
-
+const buttons = document.querySelector('.button');
+const textbox = document.querySelector('#textbox');
+ 
 function getCookie(name) {
     const value = `; ${document.cookie}`
     const parts = value.split(`; ${name}=`)
@@ -20,15 +22,19 @@ window.move_page = function(a, b) {
 window.onload = function(){
   let dm_c = getCookie('dm');
   dm = dm_c === "true";
+  dmToggle();
+}
+
+window.dmClick = function(){
+  dm = !dm;
+  document.cookie = "dm="+dm+"";
+  dmToggle();
+}
+
+window.dmToggle = function() { 
   document.getElementById("dm_icon").src = dm_imgs[+dm];
   document.body.style.backgroundColor = bgColor[+dm];
   root.style.setProperty('--main-color', textColor[+dm]);
-}
-
-window.darkMode = function() { 
-  dm = !dm;
-  document.getElementById("dm_icon").src = dm_imgs[+dm]
-  document.cookie = "dm="+dm+""
-  document.body.style.backgroundColor = bgColor[+dm];
-  root.style.setProperty('--main-color', textColor[+dm]);
+  buttons.style.setProperty("background-color", dm ? "#000074" : "#FFFFFF");
+  textbox.style.setProperty("background-color", dm ? "#000074" : "#FFFFFF");
 } 
